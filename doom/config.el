9ef74a2576bf0a2263ab40a447349c320adc5fdc
KoKo <kngzhi@hotmail.com>
2022-01-22 20:11:29 +0800

    feat: fix Chinese letter problem

--- doom/config.el
+++ doom/config.el
@@ -27,6 +27,9 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-vibrant)
 
+;; Chinese Input Problem
+(setq doom-font (font-spec :family "Sarasa Mono SC Nerd" :size 18))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org")
